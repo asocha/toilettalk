2c30c89808d34116aa6f9f1ad49ee201f06f2e5f
Jiaxin Li <jiaxinli@smu.edu>
2013-04-07 13:35:18 -0500

    Cleanup + Lastest Website Build

---
+++ Website/js/testMap.js
@@ -0,0 +1,289 @@
+var lastInfoWindow; //tracks the last info window to open
+
+window.onload = function(){
+    //initializeRoute("Dallas, TX", "Austin, TX");
+    //initializeNearby();
+    //initializeSearch("Plano, TX");
+    getRestrooms1(5);
+}
+
+//create Road Map and directions
+function initializeRoute(start, end) {
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode( { 'address': start}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            geocoder.geocode( { 'address': end}, function(results2, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    createRoute(results[0].geometry.location, results2[0].geometry.location);
+                }
+                else {
+                    alert("Sorry. We were unable to find that Destination.\nError: " + status);
+                }
+            });
+        }
+        else {
+            alert("Sorry. We were unable to find that Origin.\nError: " + status);
+        }
+    });
+}
+
+function createRoute(start, end){
+    //create route
+    var directions = {
+        origin: start,
+        destination: end,
+        travelMode: google.maps.TravelMode.DRIVING
+    };
+
+    var mapOptions = {
+        center: start,
+        zoom: 7,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    //render map
+    var renderer = new google.maps.DirectionsRenderer();
+    var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    renderer.setMap(map);
+    //renderer.setPanel(document.getElementById("directions")); //render directions
+
+    //render route overlay
+    var service = new google.maps.DirectionsService();
+    service.route(directions, function(result,status){
+        if (status === google.maps.DirectionsStatus.OK){
+            renderer.setDirections(result);
+
+            var path = result["routes"][0]["overview_path"];    //Array of LatLng that are positions along route
+
+            var restrooms = getRestrooms(start, end);   //get Restrooms from Database
+
+            for (var count = 0; restrooms && count <= restrooms.length(); count++){
+                var title = restrooms[count]['title'];
+                var stars = restrooms[count]['stars'];
+                var lat = restrooms[count]['lat'];
+                var lng = restrooms[count]['long'];
+                var icons = restrooms[count]['icons'];
+                var location = new google.maps.LatLng(lat, lng);
+innerloop:      for (index in path){
+                    if (Math.pow(lat - path[index].lat(),2) + Math.pow(lng - path[index].lng(),2) <= 0.1){
+                        var marker = new google.maps.Marker({
+                            position: location,
+                            map: map,
+                            title: title
+                        });
+                        attachInfo(map, marker, title, stars, icons);
+                        break innerloop;    //breaks the innerloop so that the same marker isn't added twice
+                    }
+                }
+            }
+        }
+    });
+}
+
+function GeoLocateSuccess(position){
+    createNearbyMap(new google.maps.LatLng(position.coords.latitude, position.coords.longitude), 1);   //geolocate user's current position
+}
+
+function GeoLocateError(msg){
+    alert("Geolocation error: " + msg.code + "\nCould not get your current location, defaulting to Dallas, Texas.");
+    createNearbyMap(new google.maps.LatLng(32.779193,-96.800537), 0);
+}
+
+//create nearby Restrooms
+function initializeNearby() {
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(GeoLocateSuccess,GeoLocateError);
+    }
+    else{
+        alert("Could not get your current location, defaulting to Dallas, Texas.");
+        createNearbyMap(new google.maps.LatLng(32.779193,-96.800537), 0);
+    }
+}
+
+function createNearbyMap(center, success){
+    var mapOptions = {
+        center: center,
+        zoom: 7,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    //render map
+    var map = new google.maps.Map(document.getElementById("map_canvas_2"), mapOptions);
+
+    var restrooms = getRestrooms(center);   //get Restrooms from Database
+
+    for (var count = 0; restrooms && count <= restrooms.length(); count++){
+        var title = restrooms[count]['title'];
+        var stars = restrooms[count]['stars'];
+        var lat = restrooms[count]['lat'];
+        var lng = restrooms[count]['long'];
+        var icons = restrooms[count]['icons'];
+        var location = new google.maps.LatLng(lat, lng);
+
+        //add if statement to check icons if we do that
+
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            title: title
+        });
+        attachInfo(map, marker, title, stars, icons);
+
+        //zoom map so only a few markers are visible
+        while (map.getBounds() && map.getBounds().contains(location) && map.getZoom() < 18){
+            map.setZoom(map.getZoom() + 1);
+        }
+    }
+    map.setZoom(map.getZoom() - 2);
+
+    //add You Are Here marker
+    if (success){
+        var marker = new google.maps.Marker({
+            position: center,
+            map: map,
+            icon: "img/you_are_here.png",
+            title: "You Are Here"
+        });
+    }
+}
+
+//create Search
+function initializeSearch(address) {
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            createSearchMap(results[0].geometry.location, address);
+        }
+        else {
+            alert("Sorry. We were unable to find that location.\nError: " + status);
+        }
+    });
+}
+
+function createSearchMap(coords, address){
+    var mapOptions = {
+        center: coords,
+        zoom: 7,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    //render map
+    var map = new google.maps.Map(document.getElementById("map_canvas_3"), mapOptions);
+
+    var restrooms = getRestrooms(coords);   //get Restrooms from Database
+
+    for (var count = 0; restrooms && count <= restrooms.length(); count++){
+        var title = restrooms[count]['title'];
+        var stars = restrooms[count]['stars'];
+        var lat = restrooms[count]['lat'];
+        var lng = restrooms[count]['long'];
+        var icons = restrooms[count]['icons'];
+        var location = new google.maps.LatLng(lat, lng);
+
+        //add if statement to check icons if we do that
+
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            title: title
+        });
+        attachInfo(map, marker, title, stars, icons);
+
+        //zoom map so only a few markers are visible
+        while (map.getBounds() && map.getBounds().contains(location) && map.getZoom() < 18){
+            map.setZoom(map.getZoom() + 1);
+        }
+    }
+    map.setZoom(map.getZoom() - 2);
+
+    var marker = new google.maps.Marker({
+        position: coords,
+        map: map,
+        icon: "img/you_are_here.png",
+        title: address
+    });
+}
+
+//add Info Window for when user clicks on a map marker
+function attachInfo(map, marker, title, stars, icons){
+    title = "<h1 class='restroomName'>" + title + "</h1>";
+
+    //title += "<img class='restroom_image' src=img/" + image + ">";
+
+    //add stars
+    for (var count = 1; count <= 5; count++){
+        if (stars >= count){
+            title += "<img class='star' src='img/star.png'>";
+        }
+        else{
+            title += "<img class='star' src='img/transparentStar.png'>";
+        }
+    }
+
+    title += "<br>";
+
+    //add icons
+    if (icons[0]){
+        title += "<img class='icon' src='img/icon_men.jpg'>";
+    }
+    if (icons[1]){
+        title += "<img class='icon' src='img/icon_women.jpg'>";
+    }
+    if (icons[2]){
+        title += "<img class='icon' src='img/icon_handicap.jpg'>";
+    }
+    if (icons[3]){
+        title += "<img class='icon' src='img/icon_24.jpg'>";
+    }
+    if (icons[4]){
+        title += "<img class='icon' src='img/icon_diaper.jpg'>";
+    }
+    if (icons[5]){
+        title += "<img class='icon' src='img/icon_pay.jpg'>";
+    }
+
+    var infoWindow = new google.maps.InfoBox({
+        content: title,
+        size: new google.maps.Size(50,50)
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+        if (lastInfoWindow){
+            lastInfoWindow.close(); //closes the last info window that opened, ensuring only one is open at a time
+        }
+        infoWindow.open(map,marker);
+        lastInfoWindow = infoWindow;
+    });
+}
+
+//query database for Restrooms nearby 1 location
+function getRestrooms1(location){
+    alert("test");
+    var request = new XMLHttpRequest();
+    request.open("GET", "phpscript/get_lat_long.php", false);
+    request.send();//location.lat(), location.lng(), 10);
+
+    if(request.status === 200){
+        alert(request.responseText);
+        var res = $.parseJSON(request.responseText);
+        return res;
+    }
+    else {
+        alert("Error Retrieving Restrooms: " + request.status);
+        return null;
+    }
+}
+
+//query database for Restrooms nearby 2 locations (start/end of a route)
+function getRestrooms2(location1, location2){
+    var request = new XMLHttpRequest();
+    request.open("GET", "phpscript/get_lat_long.php", false);
+    request.send();
+
+    if(request.status === 200){
+        return $.parseJSON(request.responseText);
+    }
+    else {
+        alert("Error Retrieving Restrooms: " + request.status);
+        return null;
+    }
+}
