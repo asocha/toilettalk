aef81cfaaa22f4306548f8d1e0431266531760ba
georgejoel <ubuntu@domU-12-31-39-0F-82-E1.compute-1.internal>
2013-04-20 18:06:54 +0000

    more updates

--- Website/js/login.js
+++ Website/js/login.js
@@ -3,6 +3,7 @@ window.onload = function(){
     document.getElementById("register").addEventListener('click',function(){register();},false);
     document.getElementById("login").addEventListener('click',function(){login();},false);
     document.getElementById("home").addEventListener('click',function(){home();},false);
+    document.getElementById("logout").addEventListener('click',function(){logout();},false);
 }
 
 function home(){
@@ -17,10 +18,18 @@ function login(){
     document.getElementById("Registerform").style.display="none";
     document.getElementById("loginform").style.display="inline";
 }
+function logout(){
+    var request = new XMLHttpRequest();
+        //request.open("GET", "http://toilettalkapiv1.apiary.io/index.php/api/toilettalkapi/session", false);
+    request.open("GET", "http://ec2-54-234-249-110.compute-1.amazonaws.com/semester_project/Team6/API_Server/index.php/api/toilettalkapi/logout", false);
+    request.send();
+    window.location = "index.html" 
 
+}
 function checklogin(){
     var request = new XMLHttpRequest();
     request.open("GET", "http://ec2-54-234-249-110.compute-1.amazonaws.com/semester_project/Team6/API_Server/index.php/api/toilettalkapi/session", false);
+    //request.open("GET", "http://toilettalkapiv1.apiary.io/index.php/api/toilettalkapi/session", false);
     request.send();
     if(request.status === 200 && request.responseText){
         console.log(request.responseText);
