97c5dbc70d1d69707a12b3e63fd238e8af29bd72
georgejoel <ubuntu@domU-12-31-39-0F-82-E1.compute-1.internal>
2013-04-21 20:00:49 +0000

    more updates to update

--- Website/js/myaccount.js
+++ Website/js/myaccount.js
@@ -5,6 +5,8 @@ window.onload = function(){
 	checklogin();
 	alert(user_id);
 	load_saved_restrooms();
+	load_saved_routes();
+	load_reviewed_restroom();
 
 
 }
@@ -16,7 +18,52 @@ function logout(){
     window.location = "index.html";
 
 }
+function load_saved_routes(){
+	var request = new XMLHttpRequest();
+    request.open("GET", "../API_Server/index.php/api/toilettalkapi/routesbyid/id/"+user_id, false);
+    request.send();
+    if(request.status === 200 && request.responseText){
+    	var jsonResponse = JSON.parse(request.responseText);
+    	console.log(request.responseText);
+    	
+    	for(var i= 0; i<jsonResponse.length;i++){
+    		
+    		var ul = document.getElementById("routes_list");
+        	var li = document.createElement('li');  
+        	li.appendChild(document.createTextNode(jsonResponse[i].origin + "\n" +jsonResponse[i].destination));
+	        //var txt=document.createTextNode('text');
+	        
+	        li.addEventListener('click',function(){alert("hi");},false);
+    		ul.appendChild(li);
+    	}
 
+
+    }
+
+}
+function load_reviewed_restroom(){
+	var request = new XMLHttpRequest();
+    request.open("GET", "../API_Server/index.php/api/toilettalkapi/restroombyid/rrid/"+user_id, false);
+    request.send();
+    if(request.status === 200 && request.responseText){
+    	var jsonResponse = JSON.parse(request.responseText);
+    	console.log(request.responseText);
+    	
+    	for(var i= 0; i<jsonResponse.length;i++){
+    		
+    		var ul = document.getElementById("review_list");
+        	var li = document.createElement('li');  
+        	li.appendChild(document.createTextNode(jsonResponse[i].restroom_id));
+	        //var txt=document.createTextNode('text');
+	        
+	        li.addEventListener('click',function(){alert("hi");},false);
+    		ul.appendChild(li);
+    	}
+
+
+    }
+
+}
 function load_saved_restrooms(){
 	var request = new XMLHttpRequest();
     request.open("GET", "../API_Server/index.php/api/toilettalkapi/savedrestrooms/id/"+user_id, false);
@@ -27,15 +74,24 @@ function load_saved_restrooms(){
     	
     	for(var i= 0; i<jsonResponse.length;i++){
     		console.log(jsonResponse[i].saved_restrooms);
-
-
+    		var ul = document.getElementById("saved_restrooms_list");
+        	var li = document.createElement('li');  
+        	li.appendChild(document.createTextNode("Restroom Id:"+jsonResponse[i].saved_restrooms));
+	        //var txt=document.createTextNode('text');
+	        
+	        li.addEventListener('click',function(){alert("hi");},false);
+    		ul.appendChild(li);
     	}
 
 
     }
 
 }
+function redirect_to_restroom(){
+	console.log('click');
 
+
+}
 function checklogin(){
     var request = new XMLHttpRequest();
     request.open("GET", "../API_Server/index.php/api/toilettalkapi/session", false);
